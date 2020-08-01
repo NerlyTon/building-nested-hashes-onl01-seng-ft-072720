e55e93451ede86471c03a234019d170476a29ede
Nerly Ton <nerlyton@gmail.com>
2020-08-01 03:38:39 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,14 +1,15 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {:patriarch => {}
+    :montague => {
+:patriarch => {}
 :matriarch => {}
 :hero => {}
 :hero_friends => []}, 
     :capulet => {
-      :patriarch => {},
-      :matriarch => {},
-      :heroine => {},
-      :heroine_friends => []
+:patriarch => {},
+:matriarch => {},
+:heroine => {},
+:heroine_friends => []
       }
     }
 end
